26571769ba736b1ecc82b46fa2c7d6857ddbb6b6
ArhamMahazra <arhammahazra@gmail.com>
2021-02-03 16:31:17 +0700

    Responsived

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ function changeCircleColor(color){
     const circle = document.querySelector('.circle');
     circle.style.background = color;
 }
+
+function menuBurger(){
+	var burger = document.querySelector('.burger');
+	var navigasi = document.querySelector('.navigasi');
+	burger.classList.toggle('active');
+	navigasi.classList.toggle('active');
+}
